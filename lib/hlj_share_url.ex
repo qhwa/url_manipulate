2c15f25f172d308bda6a1a7c3f15f7a990b7baac
qhwa <qhwa@163.com>
2016-06-09 14:54:35 +0800

    documentating

--- lib/hlj_share_url.ex
+++ lib/hlj_share_url.ex
@@ -1,4 +1,7 @@
 defmodule HljShareUrl do
+  @moduledoc """
+    Provide a function (HljShareUrl.form/2) to generate a url holding the sharing configuration data.
+  """
 
   def main(args) do
     args |> parse_args |> process
@@ -13,6 +16,23 @@ defmodule HljShareUrl do
     IO.inspect from(url, options)
   end
 
+  @doc """
+  Generate a url with sharing config.
+
+  ## Parameters
+
+    - url: String that represents the raw url
+    - setting: Keyword list representing the config. There are three keywords supported right now:
+      - title: String representing the title when sharing
+      - sub_title: String representing the sub title when sharing
+      - icon: String representing the url of icon image when sharing
+
+  ## Examples
+  
+    iex> HljShareUrl.from("http://example.com/test.html", title: "Sharing")
+    "http://example.com/test.html?title=Sharing"
+
+  """
   def from(url, setting) do
     URI.parse(url)
     |> inject_uri(setting)
